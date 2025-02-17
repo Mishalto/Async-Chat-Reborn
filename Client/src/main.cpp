addfacfd6780cdaf3f6677d5053d2409d1e07d99
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-02-17 12:36:08 +0300

    Creating a project

---
+++ Client/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main()
+{
+    std::cout << "Client\n";
+
+    return 0;
+}
---
+++ Server/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main()
+{
+    std::cout << "Server\n";
+
+    return 0;
+}
