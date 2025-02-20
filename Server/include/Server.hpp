6620edfb28e2ced170ec1d429f75eb173478c308
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-02-21 01:10:48 +0300

    Trying to ASync

--- Server/include/Server.hpp
+++ Server/include/Server.hpp
@@ -3,6 +3,7 @@
 #include <atomic>
 
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 
 using boost::asio::ip::tcp;
 
@@ -12,10 +13,14 @@ private:
     boost::asio::io_context io_context;
 
     // Server settings
-    const int server_port = 12345;  // This port is open on the server
     tcp::endpoint ep;
+    const int server_port = 12345;  // This port is open on the server
     tcp::acceptor acc;
-    std::atomic<bool> is_running;   // std::atomic is needed for multithreading, for this project just bool will be enough / будет достаточно
+    std::atomic<bool> server_is_running;   // std::atomic is needed for multithreading, for this project just bool will be enough / будет достаточно
 public:
     Server();
+
+    void start();
+    void start_accept(std::shared_ptr<tcp::socket> socket_ptr);
+    void handle_accept(std::shared_ptr<tcp::socket> socket_ptr, const boost::system::error_code& err);
 };
--- Server/src/core/Server.cpp
+++ Server/src/core/Server.cpp
@@ -1,11 +1,32 @@
 #include <Server.hpp>
 #include <iostream>
-#include <thread>
-#include <array>
-#include <string>
+#include <memory>
+#include <functional>
 
-// They must be initialized through the constructor initializer list
+// They must be initialized through/через the constructor initializer list
 Server::Server() : ep(tcp::v4(), server_port), acc(io_context, ep) {
-    is_running = false; // The default state of the server is disabled
-    std::cout << "Server initialized successfully.\n";
+    server_is_running = false; // The default state of the server is disabled
+    std::cout << server_port;
+}
+
+void Server::start() {
+    server_is_running = true;
+    std::cout << "Server is running...\n";
+    while(server_is_running) {
+        auto socket_ptr = std::make_shared<tcp::socket>(io_context);
+        start_accept(socket_ptr);
+        io_context.run();
+    }
+}
+
+void Server::start_accept(std::shared_ptr<tcp::socket> socket_ptr) {
+    acc.async_accept(*socket_ptr, boost::bind(handle_accept, socket_ptr, std::placeholders::_1));
+}
+
+void Server::handle_accept(std::shared_ptr<tcp::socket> socket_ptr, const boost::system::error_code& err) {
+    if(err) {
+        std::cerr << err.message() << '\n';
+    } else {
+        socket_ptr->async_read_some();
+    }
 }
