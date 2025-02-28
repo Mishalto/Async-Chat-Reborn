aba31e08c9e2e3a1903d2e21b1e6bf2efee447a8
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-01 01:45:13 +0300

    Mini

--- Client/src/core/Client.cpp
+++ Client/src/core/Client.cpp
@@ -1,11 +1,15 @@
 #include <Client.hpp>
 
 Client::Client() : ep_(boost::asio::ip::make_address_v4("127.0.0.1"), 12345), socket_(io_context_) {
-    std::cout << "Start connecting.\n";
+    std::cout << "Hello, waiting for connection...\n";
     Client::start();
 }
 
 void Client::start() {
-    socket_.connect(ep_);
+    boost::system::error_code err;
+    socket_.connect(ep_, err);
+    if(err) {
+        std::cerr << err.message() << '\n';
+    }
     std::cout << "Connected.\n";
 }
